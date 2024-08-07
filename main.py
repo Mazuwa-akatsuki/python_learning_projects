8b19b88d5a425cfc6be45651f6eaf8c2346e5a1e
Mazuwa-akatsuki <mazuwapmsiska@gmail.com>
2024-08-07 14:29:35 +0200

    initial project files

---
+++ main.py
@@ -0,0 +1,52 @@
+#variables 
+#a variable ca contain  number a loop , dictionary e.t.c
+#
+
+#types
+# number  0  - 9 
+# string  
+   # a stirng can contain alphanumerical characters. that is to say
+   #  Mazuwa - alphabetical charater 
+   #  Narurto12!&_uzumaki
+
+# Decimal
+
+# Boolean
+
+
+
+
+
+#FlowControl
+#Loops
+#Dictionary
+#Object Orienetd programming
+#Classes 
+#Functions 
+
+# 1+2
+# x = "Mazuwa265"
+# y = 2
+# z = input("what is your name ")
+# print("my name is :",z)
+# n = input("where do you stay ?")
+# print("you have said you live at: ",n)
+# print("thanks for providing us with your information we wil get abck tio you soon")
+# print("this is waht we have cellected from you so far?")
+# print("you said your name was : ",z)
+# print("you said your location was : 
+print("hello, world")
+if 5 > 2:
+    print("five is ggreater than two")
+    x = 5
+    y = "hello, world!"
+    
+#print("hello, world!")
+print ("cheers, mate!")
+x = 5
+y = "john"
+print(x)
+print(y)
+x = 4
+x = "sarr"
+print(x)
